134b8f18f24932ed0f26072ad0efe6b98c65313f
Z4NR34L <software@zanreal.net>
2023-06-09 12:45:28 +0200

    Fixed #4

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 export const getRouteSegments = (pathname: string) => {
-  const segments = pathname.split('/').filter((segment) => segment || segment === ''); // Keep empty segments, including the root segment
-
+  const segments = pathname.split('/').filter((segment) => segment || segment === '');
+  
   return segments.map((segment, index) => {
-    const slug = segment || 'home'; // Use '/' as the slug for the root segment
-    const url = `/${segments.slice(0, index + 1).join('/')}`; // Construct the URL for each segment
+    const slug = segment || 'home';
+    const urlSegments = segments.slice(0, index + 1);
+    const url = urlSegments.length > 1 ? `${urlSegments.join('/')}` : '/';
     const name = segment
       ? segment
-          .split('-')
-          .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
-          .join(' ')
-      : 'Home'; // Use 'Home' as the name for the root segment
-
-    return { slug, url, name };
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+      : 'Home';
+    
+    return {slug, url, name};
   });
 };
